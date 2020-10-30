0f560eb5c07981688580b30b8df508864fca5ade
Mark Jeronimus <mark.jeronimus@gmail.com>
2020-10-30 23:16:03 +0100

    Initial commit with PAOConstants

---
+++ src/org/digitalmodular/paotools/PAOConstants.java
@@ -0,0 +1,15 @@
+package org.digitalmodular.paotools;
+
+/**
+ * @author zom-b
+ */
+// Created 2020-10-30
+public interface PAOConstants {
+	int getNumColors();
+
+	int getColor(int index);
+
+	String getColorName(int index);
+
+	int getColorIndex(int rgb);
+}
